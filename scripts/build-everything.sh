cf667ff8d8aa1c1aa72e4b06232e023ee30e8fab
Mircea Hasegan <mircea@milaboratories.com>
2025-04-08 18:57:39 +0200

    multi-file asset support

--- scripts/build-everything.sh
+++ scripts/build-everything.sh
@@ -17,8 +17,8 @@ fi
 mkdir -p indexed_model
 
 # Download and process model files
-jq -r 'to_entries[] | "\(.key) \(.value.model_url)"' "$MODEL_URLS_FILE" | while read -r model_name model_url; do
-    echo "Processing $model_name from $model_url"
+jq -r 'to_entries[] | "\(.key) \(.value.model_url) \(.value.model_files[])"' "$MODEL_URLS_FILE" | while read -r model_name model_url model_file; do
+    echo "Processing $model_name from $model_url/$model_file"
     mkdir -p "indexed_model/$model_name"
-    wget -nv -c "$model_url" -O "indexed_model/$model_name/$model_name.bin"
+    wget -nv -c "$model_url/$model_file" -O "indexed_model/$model_name/$model_file"
 done
